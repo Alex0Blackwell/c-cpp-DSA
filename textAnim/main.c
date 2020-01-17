04218a96a69b2030f062bda9c16ca7e8eb36f003
alex blackwell <alex.blackwell314@gmail.com>
2020-01-16 22:49:19 -0800

    Put program in one function

--- textAnim/main.c
+++ textAnim/main.c
@@ -3,20 +3,36 @@
 #include <unistd.h>
 
 
-char getStr(char string[], int index, int len) {
-   char res[20];
+char bruteAnim(char string[]) {
+   char res[20];  // Strings of 20 in length
    strcpy(res, string);
-   
-   int a;
-   for(a = 0; a < index; a++) {
-      res[a] = string[a];
-   }
-   res[a] = string[a] - 32;
 
-   for(int b = a+1; b < len; b++) {
-      res[b] = string[b];
+   // Get string length
+   int len = 0;
+   while(res[len] != '\0') {
+      len ++; 
    }
-   printf("\r%s", res);
+
+   char temp[20];
+   for(int i = 0; i < len; i++) {
+      int a;
+      for(a = 0; a < i; a++) {
+	 // First part of string normal
+	 res[a] = string[a];
+      }
+      res[a] = string[a] - 32;  // Lower case at [i] change to upper
+
+      for(int b = a+1; b < len; b++) {
+	 // Second part of string normal
+	 res[b] = string[b];
+      }
+      printf("\r%s", res);
+   
+      sleep(1);  // Wait one second
+      fflush(stdout);
+   }
+   printf("\n");
+   
 }
 
 
@@ -24,19 +40,6 @@ int main() {
    char usrIn[20]; 
    printf("Enter a sentence: ");
    scanf("%[^\n]", usrIn);
-
-   // Get length
-   int len = 0;
-   while(usrIn[len] != '\0') {
-      len ++; 
-   }
-
-   char temp[20];
-   for(int i = 0; i < len; i++) {
-     getStr(usrIn, i, len);
-      sleep(1);  // Wait one second
-      fflush(stdout);
-   }
-   printf("\n");
+   bruteAnim(usrIn);
 }
 
