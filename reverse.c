adcd3ed4327b303bd1d87cae1607b50b40d2b72f
alex blackwell <alex.blackwell314@gmail.com>
2020-03-04 21:51:34 -0800

    initial commit

---
+++ reverse.c
@@ -0,0 +1,16 @@
+
+void reverse( int arr[], unsigned int len ) {
+    /* swap first <-> last and not middle */
+
+    int mid = len / 2;
+    int l, r;
+    l = 0;
+    r = len-1;
+
+    while(l < mid) {
+	int _temp = arr[l];
+	arr[l++] = arr[r];
+	arr[r--] = _temp;
+    }
+}
+
