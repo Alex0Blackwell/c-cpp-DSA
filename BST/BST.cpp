00f1a04a1896fed0c337816fe658bee813af30d9
alex blackwell <alex.blackwell314@gmail.com>
2020-04-17 13:31:59 -0700

    Initial commit

---
+++ BST/BST.cpp
@@ -0,0 +1,196 @@
+/* Binary Search tree */
+
+#include <iostream> 
+using namespace std; 
+  
+class BST { 
+    int data; 
+    BST *left, *right; 
+  
+    public: 
+      
+    // Default constructor. 
+    BST(); 
+      
+    // Parameterized constructor. 
+    BST(int); 
+      
+    // Insert function. 
+    BST* insert(BST *, int);
+    BST* deleteBST(BST *, int data);
+    BST* minValueBST(BST *);
+
+    // methods
+    void inorder(BST *); 
+    void preorder(BST *); 
+    void postorder(BST *); 
+    void descending(BST *); 
+}; 
+  
+
+// Default Constructor definition. 
+BST :: BST() : data(0), left(NULL), right(NULL){} 
+  
+// Parameterized Constructor definition. 
+BST :: BST(int value) { 
+    data = value; 
+    left = right = NULL; 
+} 
+  
+
+BST* BST :: insert(BST *root, int value) { 
+    if(!root) { 
+        // Insert the node, if root is NULL. 
+        return new BST(value); 
+    }  
+    // Insert data. 
+    if(value > root->data) { 
+	// traverse right side of tree
+        root->right = insert(root->right, value); 
+    } 
+    else if(value < root->data) { 
+	// traverse left side of tree
+        root->left = insert(root->left, value); 
+    }
+    // Return 'root' node, after insertion or if it already exists. 
+    return root; 
+} 
+/* // remove not working
+BST* BST::minValueBST(BST *bst) 
+{ 
+    BST* current = bst; 
+  
+    while (current && current->left != NULL) 
+        current = current->left; 
+  
+    return current; 
+}
+
+
+BST* BST::deleteBST(struct BST* root, int key) 
+{ 
+    // base case 
+    if (root == NULL)
+	return root; 
+  
+    // If the key to be deleted is smaller than the root's key, 
+    // then it lies in left subtree 
+    if (data < root->data) 
+        root->left = deleteBST(root->left, key); 
+  
+    // If the key to be deleted is greater than the root's key, 
+    // then it lies in right subtree 
+    else if (key > root->data) 
+        root->right = deleteBST(root->right, key); 
+  
+    // if key is same as root's key, then This is the node 
+    // to be deleted 
+    else
+    { 
+        // node with only one child or no child 
+        if (root->left == NULL) 
+        { 
+	    cout << "made it here\n";
+            BST *temp = root->right; 
+            free(root); 
+            return temp; 
+        } 
+        else if (root->right == NULL) 
+        { 
+            BST *temp = root->left; 
+            free(root); 
+            return temp; 
+        } 
+  
+        // node with two children: Get the inorder successor (smallest 
+        // in the right subtree) 
+        BST* temp = minValueBST(root->right); 
+  
+        // Copy the inorder successor's content to this node 
+        root->data = temp->data; 
+  
+        // Delete the inorder successor 
+        root->right = deleteBST(root->right, temp->data); 
+    } 
+    return root; 
+} 
+*/
+  
+// Inorder traversal function. 
+// This gives data in sorted order. 
+void BST::inorder(BST *root) 
+{ 
+    if(!root) 
+    { 
+        return; 
+    } 
+    inorder(root->left); 
+    cout << root->data << endl; 
+    inorder(root->right); 
+
+    return;
+} 
+
+
+// Preorder traversal function.
+// This gives data in preorder.
+void BST::preorder(BST *root) {
+    if(!root) {
+	return;
+    }
+    cout << root->data << endl;
+    preorder(root->left);
+    preorder(root->right);
+
+    return;
+}
+
+
+// Postorder traversal function.
+// This gives data in postorder.
+void BST::postorder(BST *root) {
+    if(!root) {
+	return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << endl;
+    
+    return;
+}
+
+void BST::descending(BST *root) {
+    if(!root) {
+	return;
+    }
+    descending(root->right);
+    cout << root->data << endl;
+    descending(root->left);
+    
+    return;
+}
+  
+
+// Driver code 
+int main() 
+{ 
+    BST b, *root = NULL; 
+    root = b.insert(root, 0); 
+
+    for(int i = -20; i <= 20; i += 2) {
+	b.insert(root, i);
+    }
+
+    
+    cout << "Inorder\n";
+    b.inorder(root); 
+    cout << "Preorder\n";
+    b.preorder(root); 
+    cout << "Postorder\n";
+    b.postorder(root); 
+    cout << "Descending\n";
+    b.descending(root);
+    return 0; 
+} 
+  
+
