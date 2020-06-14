ef576700d79cb29b4783b13a0b02421f92e1b6f3
alex0blackwell <alex.blackwell314@gmail.com>
2020-06-14 11:39:03 -0700

    This technically works

---
+++ definedAsBad.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+#define start int main(void)  {
+#define yell cout <<
+#define then <<
+#define newline endl;
+#define run for(int i = 0; i <
+#define times ; ++i) {
+#define moveon }
+#define end return 0; }
+
+
+start
+    yell "Welcome to some of the best code that has been written!" then newline
+    yell "...\nThis is going to be the new standard,\neveryone say it with me now!" then newline
+
+    yell "**Chanting**" then newline
+
+    run 10 times
+        yell "This is going to be the new standard!" then newline
+    moveon
+
+    yell "...\nim sorry" then newline
+end
