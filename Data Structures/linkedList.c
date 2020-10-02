aa75cfb6729b25160b1efca838d00b90637d00c8
Alex Blackwell <alex.blackwell314@gmail.com>
2020-10-01 23:43:21 -0700

    Improve: Formating change

--- Data Structures/linkedList.c
+++ Data Structures/linkedList.c
@@ -1,8 +1,8 @@
 /** This code is just for showing how pointers can make a dynamic way to
   * store memory. It does not serve as a actual linked list data structure.
-	* For a real linked list data structure, see the C++ implementation
-	* ./linkedlist/ll.cpp
-	*/
+  * For a real linked list data structure, see the C++ implementation
+  * ./linkedlist/ll.cpp
+  */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,7 +18,7 @@ void append(node *head, int num) {
   node *traverse = head;
   /* get the end of the list */
   while(traverse->next != NULL)
-		traverse = traverse->next;  // keep moving
+    traverse = traverse->next;  // keep moving
 
   /* now we have reached the current last node */
   traverse->next = (node *)malloc(sizeof(node));
@@ -33,8 +33,8 @@ void display(node *head) {
   node *traverse = head;
 
   while(traverse->next != NULL) {
-	printf("%d ", traverse->data);
-	traverse = traverse->next;
+    printf("%d ", traverse->data);
+    traverse = traverse->next;
   } printf("%d\n", traverse->data);
 
   return;
@@ -48,15 +48,15 @@ int main(void) {
     head->data = 1;
     head->next = NULL;
 
-		printf("Append the number 5\n");
+    printf("Append the number 5\n");
     append(head, 5);
     display(head);
 
-		printf("Append the number 3\n");
+    printf("Append the number 3\n");
     append(head, 3);
     display(head);
 
-		free(head);  // avoid a memory leak
+    free(head);  // avoid a memory leak
 
     return 0;
 }
